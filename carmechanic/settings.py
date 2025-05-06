4aa3045868c89c6898c68c046a5f8ff0fd3f960f
Physics23 <laye192000@yahoo.fr>
2025-05-06 13:31:10 -0500

    first commit

--- carmechanic/settings.py
+++ carmechanic/settings.py
@@ -136,10 +136,19 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+#STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [BASE_DIR /'static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'static/media'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
